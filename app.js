9c12a9e437b12c3b379d63a5e1ccd246d4bacb21
YexChen <76753500@qq.com>
2017-11-07 21:21:22 +0800

    项目初始化

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 App({
-  
+  onLauch(){
+    console.log("加载了");
+  },
+  onShow(){
+    console.log("显示了");
+  },
+  onHide(){
+    console.log("隐藏了");
+  },
+  onError(){
+    console.log("出错了");
+  }
 })
